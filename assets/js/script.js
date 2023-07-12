f15a06de56254ac5770f39be3fff5a0a24c78c55
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-11 22:29:06 -0500

    Added pseudo coding for both HTML and most of javascript, and added header and search function for city

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// var for the key and api URL (with the lat and long )
+// var for city to collect whatever user inputs
+// var for the city name api URL with parameters q (for the city variable) and appid (for the api key)
+// Possible example: var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+// search button click event listener
+    // fetch function to get API
+    // We'll want to get the cities lat and log
+
+    // function to save user search to local storage
+    
+    // dayjs to render weather by date/time accuracy on the page
+    // api link for each city
+    
+    // var link lat and long
+    
+    // var cityURL 
+    // user inputs city it goes into cityURL
+    // from cityURL we'll take the lat and long using 
+
+
+// function to render it to the page

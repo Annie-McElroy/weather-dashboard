a53b1a736d0afd5ac0a8577acfd55c4741907f87
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-12 09:38:38 -0500

    Started writing javascript with adding a variable for the apikey, city url, and city variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,19 @@
 // var for the key and api URL (with the lat and long )
+var APIKey = "dba807256a5277a928e9bb4daa57671f"
+
+"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
 // var for city to collect whatever user inputs
 // var for the city name api URL with parameters q (for the city variable) and appid (for the api key)
 // Possible example: var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
 // var cityURL 
-// parament q and appid
+var cityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
+
 // variable for city
+var city;
+
+// parament q and appid
 // user inputs city it goes into cityURL
 // from cityURL we'll take the lat and long using 
 
@@ -20,11 +27,14 @@
 
     // function to save user search to local storage
     
-    // dayjs to render weather by date/time accuracy on the page
     // api link for each city
     
     // var link lat and long
     
+    
+    
+
 
 
 // function to render it to the page
+// dayjs to render weather by date/time accuracy on the page

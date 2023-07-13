b7022ecfaa6e74819d79fef05ffa2e8eb9cb66a0
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-12 20:35:21 -0500

    Fixed card sizing on html for the 5 day forcast, added basic search function for city to add that city to local storage and began making fucntion to append the city from local storage into querry URL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,62 @@
+// variables for search button
+var searchFormEl = document.querySelector('#search-form')
+var searchBtn = document.querySelector("#search-button")
+
 // var for the key and api URL (with the lat and long )
 var APIKey = "dba807256a5277a928e9bb4daa57671f"
 
-"https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
-// var for city to collect whatever user inputs
+// "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+
 // var for the city name api URL with parameters q (for the city variable) and appid (for the api key)
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 // Possible example: var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 
 // var cityURL 
-var cityURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey
+
 
 // variable for city
-var city;
-
-// parament q and appid
-// user inputs city it goes into cityURL
-// from cityURL we'll take the lat and long using 
-
-
-
+// var for city to collect whatever user inputs
 
 
 // search button click event listener
+function searchCity(event) {
+    event.preventDefault();
+    
+    var cityInputVal = document.querySelector('#city-input').value;
+    
+    if (!cityInputVal) {
+        var errorMsg = document.querySelector('#error-msg')
+        errorMsg.textContent = "You must enter a city name!"
+        return;
+    }
+    
+    localStorage.setItem("City", cityInputVal)
+    // console.log(cityInputVal)
+    
+}
+
+// searchBtn 
+searchFormEl.addEventListener('submit', searchCity);
+
+
+// function that searches the city name within city name API URL
+
+// grab city from localstorage and put it into city URL
+// user inputs city it goes into cityURL
+// from cityURL we'll take the lat and long with variable for document.location.lat
+
+function appendCity() {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    var city = localStorage.getItem("City", cityInputVal)
+
+}
+
+
+
+
+
     // fetch function to get API
     // We'll want to get the cities lat and log
 
@@ -33,7 +68,12 @@ var city;
     
     
     
+// Search function for the API
+// function searchApi(query) {
+//     var queryURL = 'api.openweathermap.org/data/2.5/forecast?'
 
+//     if 
+// }
 
 
 // function to render it to the page

caa22130e6fc29878e7569bbba33f24bc3079e96
Annie McElroy <134727217+Annie-McElroy@users.noreply.github.com>
2023-07-14 22:33:48 -0500

    Fixed render data to display correct weather icon and fixed current day image size

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,6 @@ var APIKey = "dba807256a5277a928e9bb4daa57671f";
 
 // search button click event listener
 function searchCity(event) {
-    event.preventDefault();
     
     var cityInputVal = cityInput.value;
     userSearch.push(cityInputVal);
@@ -39,16 +38,25 @@ function searchCity(event) {
     if (!cityInputVal) {
         var errorMsg = document.querySelector('#error-msg')
         errorMsg.textContent = "You must enter a city name!";
-
+        
         return;
     }
     
     // function to save user search to local storage
     localStorage.setItem("City", JSON.stringify(userSearch));
     console.log(userSearch);
-    searchHistory()
-    
-}
+
+    day1.innerHTML = " ";
+    day2.innerHTML = " ";
+    day3.innerHTML = " ";
+    day4.innerHTML = " ";
+    day5.innerHTML = " ";
+    currentCard.innerHTML = " ";
+
+    searchHistory(cityInputVal);
+    event.preventDefault();
+
+};
 // searchBtn 
 searchFormEl.addEventListener('submit', searchCity);
 
@@ -56,11 +64,11 @@ searchFormEl.addEventListener('submit', searchCity);
 
 function searchHistory() {
     var savedPlaces = JSON.parse(localStorage.getItem("City"));
-
+    
     if (savedPlaces !== null) {
         userSearch = savedPlaces;
     }
-
+    
     for (i = 0; i < savedPlaces.length; i++) {
         var historyDisplay = savedPlaces[i];
         var cityBtn = document.createElement("button");
@@ -69,17 +77,26 @@ function searchHistory() {
     cityBtn.textContent = historyDisplay;
     cityBtn.setAttribute("data-index", i);
     savedCities.appendChild(cityBtn);
+    
+    function clickHistory() {
+        searchFormEl.reset();
+        // city = historyDisplay
+        // console.log(city)
+        // appendCity(city)
+    }
+    cityBtn.addEventListener("click", clickHistory)
 
+    
     appendCity()
 }
 
+
+
+
 // function that searches the city name within city name API URL
-
-
-
-function appendCity() {
+function appendCity(city) {
     // user inputs city it goes into cityURL
-    var city = document.querySelector('#city-input').value
+    city = document.querySelector('#city-input').value
     var cityURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + APIKey;
     
     console.log(city)
@@ -98,14 +115,11 @@ function appendCity() {
             console.log(data);
             var lat = (data[0].lat);
             var lon = (data[0].lon);
-            console.log (lat);
+            console.log(lat);
             console.log(lon);
             searchApi(lat, lon);
         });
 }
-
-
-
 
 
 
@@ -128,88 +142,66 @@ function searchApi(lat, lon) {
         .then (function (data) {
             console.log(data);
 
+
             // current date data
             var timestamp = (data.daily[0].dt);
-            var weatherIconData = (data.daily[0].weather[0].id)
+            var weatherIconData = 'https://openweathermap.org/img/wn/' + (data.daily[0].weather[0].icon) + '@2x.png';
             var tempData = (data.daily[0].temp.day);
             var windData = (data.daily[0].wind_speed);
             var humidityData = (data.daily[0].humidity)
-            console.log(timestamp)
+            // console.log(timestamp)
             console.log(weatherIconData);
-            console.log(tempData);
-            console.log(windData);
-            console.log(humidityData);
+            // console.log(tempData);
+            // console.log(windData);
+            // console.log(humidityData);
 
             renderCurrentDate(timestamp, weatherIconData, tempData, windData, humidityData);
 
             // day 1 data
             var timestamp1 = (data.daily[1].dt);
-            var weatherIconData1 = (data.daily[1].weather[0].id)
+            var weatherIconData1 = 'https://openweathermap.org/img/wn/' + (data.daily[1].weather[0].icon) + '@2x.png';
             var tempData1 = (data.daily[1].temp.day);
             var windData1 = (data.daily[1].wind_speed);
             var humidityData1 = (data.daily[1].humidity)
-            console.log(timestamp1)
-            console.log(weatherIconData1);
-            console.log(tempData1);
-            console.log(windData1);
-            console.log(humidityData1);
+            console.log(weatherIconData1)
             
             renderDay1(timestamp1, weatherIconData1, tempData1, windData1, humidityData1)
 
             // day 2 data
             var timestamp2 = (data.daily[2].dt);
-            var weatherIconData2 = (data.daily[2].weather[0].id)
+            var weatherIconData2 = 'https://openweathermap.org/img/wn/' + (data.daily[2].weather[0].icon) + '@2x.png';
             var tempData2 = (data.daily[2].temp.day);
             var windData2 = (data.daily[2].wind_speed);
             var humidityData2 = (data.daily[2].humidity)
-            console.log(timestamp2)
-            console.log(weatherIconData2);
-            console.log(tempData2);
-            console.log(windData2);
-            console.log(humidityData2);
             
             renderDay2(timestamp2, weatherIconData2, tempData2, windData2, humidityData2)
 
 
             // day 3 data
             var timestamp3 = (data.daily[3].dt);
-            var weatherIconData3 = (data.daily[3].weather[0].id)
+            var weatherIconData3 = 'https://openweathermap.org/img/wn/' + (data.daily[3].weather[0].icon) + '@2x.png';
             var tempData3 = (data.daily[3].temp.day);
             var windData3 = (data.daily[3].wind_speed);
             var humidityData3 = (data.daily[3].humidity)
-            console.log(timestamp3)
-            console.log(weatherIconData3);
-            console.log(tempData3);
-            console.log(windData3);
-            console.log(humidityData3);
             
             renderDay3(timestamp3, weatherIconData3, tempData3, windData3, humidityData3);
 
             // day 4 data
             var timestamp4 = (data.daily[4].dt);
-            var weatherIconData4 = (data.daily[4].weather[0].id)
+            var weatherIconData4 = 'https://openweathermap.org/img/wn/' + (data.daily[4].weather[0].icon) + '@2x.png';
             var tempData4 = (data.daily[4].temp.day);
             var windData4 = (data.daily[4].wind_speed);
             var humidityData4 = (data.daily[4].humidity)
-            console.log(timestamp4)
-            console.log(weatherIconData4);
-            console.log(tempData4);
-            console.log(windData4);
-            console.log(humidityData4);
             
             renderDay4(timestamp4, weatherIconData4, tempData4, windData4, humidityData4);
 
             // day 5 data
             var timestamp5 = (data.daily[5].dt);
-            var weatherIconData5 = (data.daily[5].weather[0].id)
+            var weatherIconData5 = 'https://openweathermap.org/img/wn/' + (data.daily[5].weather[0].icon) + '@2x.png';
             var tempData5 = (data.daily[5].temp.day);
             var windData5 = (data.daily[5].wind_speed);
             var humidityData5 = (data.daily[5].humidity)
-            console.log(timestamp5)
-            console.log(weatherIconData5);
-            console.log(tempData5);
-            console.log(windData5);
-            console.log(humidityData5);
+
             
             renderDay5(timestamp5, weatherIconData5, tempData5, windData5, humidityData5);
             
@@ -223,27 +215,28 @@ function renderCurrentDate(timestamp, weatherIconData, tempData, windData, humid
     var dateFormat = new Date(timestamp * 1000);
     var date = dateFormat.toLocaleDateString();
     console.log(date);
+
+    
     
     // Write a variable to add input city to appear next to givenDate
     var givenDate = document.querySelector("#city-header");
-    var weatherIcon = document.createElement('li');
+    var weatherIcon = document.querySelector("#weather-emoji");
+    weatherIcon.setAttribute("src", weatherIconData)
     var temp = document.createElement('li');
     var wind = document.createElement('li');
     var humidity = document.createElement('li');
 
-    currentCard.append(weatherIcon, temp, wind, humidity);
+    currentCard.append(temp, wind, humidity);
     
     givenDate.textContent = date;
-    weatherIcon.textContent = weatherIconData;
+    // weatherIcon.textContent = weatherIconData;
     temp.textContent = tempData;
     wind.textContent = windData;
     humidity.textContent = humidityData;
     
 }
 
-// day 1 display data
-// day 1 display data
-// day 1 display data
+
 // day 1 display data
 function renderDay1(timestamp1, weatherIconData1, tempData1, windData1, humidityData1) {
 
@@ -252,15 +245,16 @@ function renderDay1(timestamp1, weatherIconData1, tempData1, windData1, humidity
     console.log(date);
 
     var dayDate = document.querySelector('#date-1')
-    var weatherIcon = document.createElement('li');
+    var weatherIcon = document.querySelector("#weather-emoji-1");
+    weatherIcon.setAttribute("src", weatherIconData1)
     var temp = document.createElement('li');
     var wind = document.createElement('li');
     var humidity = document.createElement('li');
     
-    day1.append(weatherIcon, temp, wind, humidity);
+    day1.append(temp, wind, humidity);
     
     dayDate.textContent = date
-    weatherIcon.textContent = weatherIconData1;
+    // weatherIcon.textContent = weatherIconData1;
     temp.textContent = tempData1;
     wind.textContent = windData1;
     humidity.textContent = humidityData1;
@@ -275,15 +269,16 @@ function renderDay2(timestamp2, weatherIconData2, tempData2, windData2, humidity
     console.log(date);
 
     var dayDate = document.querySelector('#date-2')
-    var weatherIcon = document.createElement('li');
+    var weatherIcon = document.querySelector("#weather-emoji-2");
+    weatherIcon.setAttribute("src", weatherIconData2)
     var temp = document.createElement('li');
     var wind = document.createElement('li');
     var humidity = document.createElement('li');
 
-    day2.append(weatherIcon, temp, wind, humidity);
+    day2.append(temp, wind, humidity);
 
     dayDate.textContent = date
-    weatherIcon.textContent = weatherIconData2;
+    // weatherIcon.textContent = weatherIconData2;
     temp.textContent = tempData2;
     wind.textContent = windData2;
     humidity.textContent = humidityData2;
@@ -297,15 +292,16 @@ function renderDay3(timestamp3, weatherIconData3, tempData3, windData3, humidity
     console.log(date);
 
     var dayDate = document.querySelector('#date-3')
-    var weatherIcon = document.createElement('li');
+    var weatherIcon = document.querySelector("#weather-emoji-3");
+    weatherIcon.setAttribute("src", weatherIconData3)
     var temp = document.createElement('li');
     var wind = document.createElement('li');
     var humidity = document.createElement('li');
 
-    day3.append(weatherIcon, temp, wind, humidity);
+    day3.append(temp, wind, humidity);
 
     dayDate.textContent = date
-    weatherIcon.textContent = weatherIconData3;
+    // weatherIcon.textContent = weatherIconData3;
     temp.textContent = tempData3;
     wind.textContent = windData3;
     humidity.textContent = humidityData3;
@@ -319,15 +315,16 @@ function renderDay4(timestamp4, weatherIconData4, tempData4, windData4, humidity
     console.log(date);
 
     var dayDate = document.querySelector('#date-4')
-    var weatherIcon = document.createElement('li');
+    var weatherIcon = document.querySelector("#weather-emoji-4");
+    weatherIcon.setAttribute("src", weatherIconData4)
     var temp = document.createElement('li');
     var wind = document.createElement('li');
     var humidity = document.createElement('li');
 
-    day4.append(weatherIcon, temp, wind, humidity);
+    day4.append(temp, wind, humidity);
 
     dayDate.textContent = date
-    weatherIcon.textContent = weatherIconData4;
+    // weatherIcon.textContent = weatherIconData4;
     temp.textContent = tempData4;
     wind.textContent = windData4;
     humidity.textContent = humidityData4;
@@ -341,15 +338,16 @@ function renderDay5(timestamp5, weatherIconData5, tempData5, windData5, humidity
     console.log(date);
 
     var dayDate = document.querySelector('#date-5');
-    var weatherIcon = document.createElement('li');
+    var weatherIcon = document.querySelector("#weather-emoji-5");
+    weatherIcon.setAttribute("src", weatherIconData5)
     var temp = document.createElement('li');
     var wind = document.createElement('li');
     var humidity = document.createElement('li');
 
-    day5.append(weatherIcon, temp, wind, humidity);
+    day5.append(temp, wind, humidity);
 
     dayDate.textContent = date;
-    weatherIcon.textContent = weatherIconData5;
+    // weatherIcon.textContent = weatherIconData5;
     temp.textContent = tempData5;
     wind.textContent = windData5;
     humidity.textContent = humidityData5;
